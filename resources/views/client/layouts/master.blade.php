cf4674c35b5f35116f883f537f2dcba099fecfde
nguyenxuanmin <nguyenxuanmin1998@gmail.com>
2025-01-20 21:05:36 +0700

    create new layout

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -6,81 +6,16 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>@yield('title')</title>
         <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="{{asset('library/css/style.css')}}">
-		@vite(['resources/css/app.css','resources/js/app.js'])
-        <script src="https://cdn.ckeditor.com/ckeditor5/41.3.1/classic/ckeditor.js"></script>
-		@livewireStyles
-		<style>
-            @import url('https://fonts.googleapis.com/css2?family=Oregano:ital@0;1&display=swap');
-            .work-sans {
-                font-family: 'Work Sans', sans-serif;
-            }
-                    
-            #menu-toggle:checked + #menu {
-                display: block;
-            }
-            
-            .hover\:grow {
-                transition: all 0.3s;
-                transform: scale(1);
-            }
-            
-            .hover\:grow:hover {
-                transform: scale(1.02);
-            }
-            
-            .carousel-open:checked + .carousel-item {
-                position: static;
-                opacity: 100;
-            }
-            
-            .carousel-item {
-                -webkit-transition: opacity 0.6s ease-out;
-                transition: opacity 0.6s ease-out;
-            }
-            
-            #carousel-1:checked ~ .control-1,
-            #carousel-2:checked ~ .control-2,
-            #carousel-3:checked ~ .control-3 {
-                display: block;
-            }
-            
-            .carousel-indicators {
-                list-style: none;
-                margin: 0;
-                padding: 0;
-                position: absolute;
-                bottom: 2%;
-                left: 0;
-                right: 0;
-                text-align: center;
-                z-index: 10;
-            }
-            
-            #carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
-            #carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
-            #carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
-                color: #000;
-                /*Set to match the Tailwind colour you want the active one to be */
-            }
-        </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        @vite(['resources/css/style.css'])
 	</head>
-	<body class="bg-gray-300 font-sans leading-normal text-base tracking-normal">
-        <div class="mx-auto max-w-full md:max-w-7xl md:px-4 lg:px-8">
-            @include('client.layouts.menu')
-            @yield('content')
-            @include('client.layouts.footer')
-            @livewire('wire-elements-modal')
-        </div>
+	<body>
+        @include('client.layouts.menu')
+        @yield('content')
     </body>
-    @livewireScripts
-    <script>
-        function updateCartNumber(number){
-            const cart_count = document.getElementById("cart-count")
-            if(cart_count){
-                cart_count.innerText = number
-            }
-        }
-    </script>
-	<script src="{{asset('library/js/app.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </html>
