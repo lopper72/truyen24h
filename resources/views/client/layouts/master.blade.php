c14a0b6e496e1d0c230f27c283e293f97235aae9
lopper72 <vanphuong.fit@gmail.com>
2025-01-22 11:32:22 +0700

    fix css js

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Playwrite+IN:wght@100..400&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-		@vite(['resources/css/main.css'])
+        <link rel="stylesheet" href="{{ asset('cssjs/main.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -23,7 +23,7 @@
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    @vite(['resources/js/main.js'])
+    <script src="{{ asset('cssjs/main.js') }}">
     <script>
         function showMenuMobile() {
             $("#menuMobile").addClass("showMenu");
